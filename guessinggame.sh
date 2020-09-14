28064cf84da91245c0cf38a51cc0b52407eeb7bb
amanguptaBay <47506369+amanguptaBay@users.noreply.github.com>
2020-09-13 21:18:50 -0700

    Added in loopCounting, hints for guess

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,23 @@
-count=$(ls -p | grep -v / | wc -l)
+function getFileCount {
+  c=$(ls -p | grep -v / | wc -l)
+  echo $c
+}
+
+fileCount=$(getFileCount)
 guess="-1"
-while [[ $guess -ne $count ]]
+loopCount=0
+
+while [[ $guess -ne $fileCount ]]
 do
+  let loopCount=$loopCount+1
   echo "Enter a guess for the amount of files"
+  echo -n "Guess $loopCount: "
   read guess
+  if [[ $guess -gt $fileCount ]]; then
+    echo "The guess was too high, try again!"
+  elif [[ $guess -lt $fileCount ]]; then
+    echo "The guess was too low, try again!"
+  fi
 done
 echo "You got it!"
+echo "Took $loopCount tries"
